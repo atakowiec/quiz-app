237c76c632c202fff2442fe460637df9b3041cf5
wyntia <gosia.chyrza@gmail.com>
2024-09-09 22:47:44 +0200

    header frontend

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "../styles/Header.scss";
+import { IoPersonCircleOutline } from "react-icons/io5";
+import { IoMdNotificationsOutline } from "react-icons/io";
+import { IoInformationCircleOutline } from "react-icons/io5";
+
+const Header = () => {
+  return (
+    <>
+      <header className="header-top-strip p-1">
+        <div className="header-content">
+          <div className="logo">Quiz</div>
+          <div className="icon-container">
+            <a href="/" className="info gap-15">
+              <IoInformationCircleOutline color="white" size="30px" />
+            </a>
+            <a href="/" className="notifications gap-15">
+              <IoMdNotificationsOutline color="white" size="30px" />
+            </a>
+            <a href="/" className="profile gap-15">
+              <IoPersonCircleOutline color="white" size="30px" />
+            </a>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
