2cb9dfea3f13402e7685d440eec48a62e80bba59
wyntia <gosia.chyrza@gmail.com>
2024-09-13 19:25:12 +0200

    Profile frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,32 @@
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
-import Layout from './components/Layout'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout";
 import "./styles/Global.scss";
-import Login from './pages/Login';
-import Register from './pages/Register';
-import CreateGame from './pages/CreateGame';
-import {Provider} from "react-redux";
-import {store} from "./store";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import CreateGame from "./pages/CreateGame";
+import Profile from "./pages/Profile.tsx";
+import { Provider } from "react-redux";
+import { store } from "./store";
 import Logout from "./pages/Logout.tsx";
 
 function App() {
-
   return (
     <>
       <Provider store={store}>
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Layout/>}>
-              <Route path='logout' element={<Logout/>}/>
-              <Route path='login' element={<Login/>}/>
-              <Route path='register' element={<Register/>}/>
-              <Route path='/create-game' element={<CreateGame/>}/>
+            <Route path="/" element={<Layout />}>
+              <Route path="logout" element={<Logout />} />
+              <Route path="login" element={<Login />} />
+              <Route path="register" element={<Register />} />
+              <Route path="create-game" element={<CreateGame />} />
+              <Route path="profile" element={<Profile />} />
             </Route>
           </Routes>
         </BrowserRouter>
       </Provider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
