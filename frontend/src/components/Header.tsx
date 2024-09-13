65d816ce7d5f43be25ba29b55f36365c7a615ca9
wyntia <gosia.chyrza@gmail.com>
2024-09-13 19:43:05 +0200

    added logged in page change

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,15 +1,31 @@
-import styles from  "../styles/Header.module.scss";
+import styles from "../styles/Header.module.scss";
 import { IoPersonCircleOutline } from "react-icons/io5";
 import { IoMdNotificationsOutline } from "react-icons/io";
 import { IoInformationCircleOutline } from "react-icons/io5";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { State } from "../store";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const navigate = useNavigate();
+  const user = useSelector<State>((state: State) => state.user) as any;
+
+  const handleIconClick = () => {
+    if (user.loggedIn) {
+      navigate("/profile");
+    } else {
+      navigate("/login");
+    }
+  };
+
   return (
     <>
       <header className={`${styles.headerTopStrip} p-1`}>
         <div className={styles.headerContent}>
-          <Link to="/" className={styles.logo} >Quiz</Link>
+          <Link to="/" className={styles.logo}>
+            Quiz
+          </Link>
           <div className={styles.iconContainer}>
             <Link to="/" className={`info ${styles.gap15}`}>
               <IoInformationCircleOutline color="white" size="30px" />
@@ -17,9 +33,12 @@ const Header = () => {
             <Link to="/" className={`notifications ${styles.gap15}`}>
               <IoMdNotificationsOutline color="white" size="30px" />
             </Link>
-            <Link to="/login" className={`profile ${styles.gap15}`}>
+            <div
+              className={`${styles.profile} ${styles.gap15}`}
+              onClick={handleIconClick}
+            >
               <IoPersonCircleOutline color="white" size="30px" />
-            </Link>
+            </div>
           </div>
         </div>
       </header>
